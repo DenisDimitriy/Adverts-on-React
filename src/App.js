46eb4dd89fc06f571bd2e97bdfb0251ff3e6b483
Denis <denis.dimitriy@gmail.com>
2018-02-12 00:47:30 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
+import About from './About'
+import Home from './Home'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+/** Router from https://reacttraining.com/react-router/web/example/basic */
 
 export default class App extends Component{
     render() {
         return(
-            <div>
-                <h1>Header</h1>
-            </div>
+            <Router>
+                <div>
+                <ul>
+                    <li>
+                    <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                    <Link to="/about">About</Link>
+                    </li>
+                </ul>
+
+                <hr />
+
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                </div>
+            </Router>
         )
     }
 }
-
